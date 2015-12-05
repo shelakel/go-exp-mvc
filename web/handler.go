90d1087c8ec5a3dbef57772e2d5c397afe5c884f
Shelakel <shelah.kell@gmail.com>
2015-12-05 12:02:23 +0200

    Refactor into a Context struct with spread

--- web/handler.go
+++ web/handler.go
@@ -8,22 +8,55 @@ import (
 	"golang.org/x/net/context"
 )
 
-// Handler is a http.Handler with a net/context.Context parameter.
-type Handler interface {
-	ServeHTTP(c context.Context, w http.ResponseWriter, r *http.Request)
+// Context represents a HTTP Context.
+type Context struct {
+	Context  context.Context
+	Request  *http.Request
+	Response http.ResponseWriter
 }
 
-// HandlerFunc implements Handler and Middleware.
-type HandlerFunc func(c context.Context, w http.ResponseWriter, r *http.Request)
+// Handler is a http.Handler with a net/context.Context parameter.
+type Handler interface {
+	ServeHTTP(c *Context)
+}
 
-func (f HandlerFunc) ServeHTTP(c context.Context, w http.ResponseWriter, r *http.Request) {
-	f(c, w, r)
+// HandlerFunc implements Handler.
+type HandlerFunc func(c *Context)
+
+func (f HandlerFunc) ServeHTTP(c *Context) {
+	f(c)
+}
+
+// HandlerSpreadFunc implements Handler.
+type HandlerSpreadFunc func(c context.Context, w http.ResponseWriter, r *http.Request)
+
+func (f HandlerSpreadFunc) ServeHTTP(c *Context) {
+	f(c.Context, c.Response, c.Request)
+}
+
+// ResultHandler is a Handler returning a Handler (action result)
+type ResultHandler interface {
+	ServeHTTP(c *Context) Handler
+}
+
+// ResultHandlerFunc implements ResultHandler.
+type ResultHandlerFunc func(c *Context) Handler
+
+func (f ResultHandlerFunc) ServeHTTP(c *Context) Handler {
+	return f(c)
+}
+
+// ResultHandlerSpreadFunc implements ResultHandler.
+type ResultHandlerSpreadFunc func(c context.Context, w http.ResponseWriter, r *http.Request) Handler
+
+func (f ResultHandlerSpreadFunc) ServeHTTP(c *Context) Handler {
+	return f(c.Context, c.Response, c.Request)
 }
 
 // ComposeMiddleware combines Middleware into a single Middleware function
 func ComposeMiddleware(middleware ...interface{}) func(interface{}) Handler {
 	if middleware == nil || len(middleware) == 0 {
-		return convertHandler
+		return convertHandlerPanic
 	}
 	// convert before returning to fail early at ComposeMiddleware call
 	adapters := make([]func(Handler) Handler, len(middleware))
@@ -34,7 +67,7 @@ func ComposeMiddleware(middleware ...interface{}) func(interface{}) Handler {
 		adapters[i] = convertMiddleware(middleware, i)
 	}
 	return func(v interface{}) Handler {
-		next := convertHandler(v)
+		next := convertHandlerPanic(v)
 		for i := len(adapters) - 1; i >= 0; i-- {
 			next = adapters[i](next)
 		}
@@ -50,22 +83,14 @@ func convertMiddleware(middleware []interface{}, i int) func(Handler) Handler {
 		return composeMiddlewareAdapter(mw)
 	case func(Handler) Handler:
 		return mw
-	case Handler:
-		return handlerMiddlewareAdapter(mw)
-	case HandlerFunc:
-		return handlerMiddlewareAdapter(mw)
-	case func(context.Context, http.ResponseWriter, *http.Request):
-		return handlerMiddlewareAdapter(HandlerFunc(mw))
 	case func(http.Handler) http.Handler:
 		return httpMiddlewareAdapter(mw)
-	case http.Handler:
-		return httpHandlerMiddlewareAdapter(mw)
-	case http.HandlerFunc:
-		return httpHandlerMiddlewareAdapter(mw)
-	case func(http.ResponseWriter, *http.Request):
-		return httpHandlerMiddlewareAdapter(http.HandlerFunc(mw))
 	default:
-		panic(fmt.Sprintf("Unsupported middleware type '%v' at index %d.", reflect.TypeOf(middleware[i]), i))
+		h, err := convertHandler(mw)
+		if err != nil {
+			panic(fmt.Sprintf("Unsupported middleware type '%v' at index %d.", reflect.TypeOf(middleware[i]), i))
+		}
+		return handlerMiddlewareAdapter(h)
 	}
 }
 
@@ -77,63 +102,83 @@ func composeMiddlewareAdapter(mw func(interface{}) Handler) func(Handler) Handle
 
 func handlerMiddlewareAdapter(mw Handler) func(Handler) Handler {
 	return func(next Handler) Handler {
-		if next == nil {
-			return mw
-		}
-		return HandlerFunc(func(c context.Context, w http.ResponseWriter, r *http.Request) {
-			mw.ServeHTTP(c, w, r)
-			next.ServeHTTP(c, w, r)
-		})
-	}
-}
-
-func httpHandlerMiddlewareAdapter(mw http.Handler) func(Handler) Handler {
-	return func(next Handler) Handler {
-		return HandlerFunc(func(c context.Context, w http.ResponseWriter, r *http.Request) {
-			mw.ServeHTTP(w, r)
-			next.ServeHTTP(c, w, r)
+		return HandlerFunc(func(c *Context) {
+			mw.ServeHTTP(c)
+			next.ServeHTTP(c)
 		})
 	}
 }
 
 func httpMiddlewareAdapter(mw func(http.Handler) http.Handler) func(Handler) Handler {
 	return func(next Handler) Handler {
-		return HandlerFunc(func(c context.Context, w http.ResponseWriter, r *http.Request) {
-			mw(http.HandlerFunc(func(w1 http.ResponseWriter, r1 *http.Request) {
-				next.ServeHTTP(c, w1, r1)
-			})).ServeHTTP(w, r)
+		return HandlerFunc(func(c *Context) {
+			mw(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				c.Response = w
+				c.Request = r
+				next.ServeHTTP(c)
+			})).ServeHTTP(c.Response, c.Request)
 		})
 	}
 }
 
 // handler
 
-func convertHandler(v interface{}) Handler {
+func convertHandlerPanic(v interface{}) Handler {
+	h, err := convertHandler(v)
+	if err != nil {
+		panic(err)
+	}
+	return h
+}
+
+func convertHandler(v interface{}) (Handler, error) {
 	if v == nil {
-		panic(fmt.Sprintf("Handler of type '%v' is nil.", reflect.TypeOf(v)))
+		return nil, fmt.Errorf("Handler of type '%v' is nil.", reflect.TypeOf(v))
 	}
 	switch h := v.(type) {
-	case Handler:
-		return h
-	case HandlerFunc:
-		return h
+	case Handler: // HandlerFunc, HandlerSpreadFunc implements Handler
+		return h, nil
+	case func(*Context):
+		return HandlerFunc(h), nil
 	case func(context.Context, http.ResponseWriter, *http.Request):
-		return HandlerFunc(h)
-	case http.Handler:
-		return httpHandlerAdapter(h)
-	case http.HandlerFunc:
-		return httpHandlerAdapter(h)
+		return handlerSpreadAdapter(h), nil
+	case http.Handler: // http.HandlerFunc implements http.Handler
+		return httpHandlerAdapter(h), nil
 	case func(http.ResponseWriter, *http.Request):
-		return httpHandlerAdapter(http.HandlerFunc(h))
+		return httpHandlerAdapter(http.HandlerFunc(h)), nil
+	case ResultHandler: // ResultHandlerFunc, ResultHandlerSpreadFunc implements ResultHandler
+		return resultHandlerAdapter(h), nil
+	case func(*Context) Handler:
+		return resultHandlerAdapter(ResultHandlerFunc(h)), nil
+	case func(context.Context, http.ResponseWriter, *http.Request) Handler:
+		return resultHandlerSpreadAdapter(h), nil
 	default:
-		panic(fmt.Sprintf("Unsupported handler type '%v'", reflect.TypeOf(h)))
+		return nil, fmt.Errorf("Unsupported handler type '%v'", reflect.TypeOf(h))
 	}
 }
 
 // convert handler adapters
 
+func handlerSpreadAdapter(h func(c context.Context, w http.ResponseWriter, r *http.Request)) Handler {
+	return HandlerFunc(func(c *Context) {
+		h(c.Context, c.Response, c.Request)
+	})
+}
+
 func httpHandlerAdapter(h http.Handler) Handler {
-	return HandlerFunc(func(c context.Context, w http.ResponseWriter, r *http.Request) {
-		h.ServeHTTP(w, r)
+	return HandlerFunc(func(c *Context) {
+		h.ServeHTTP(c.Response, c.Request)
+	})
+}
+
+func resultHandlerAdapter(h ResultHandler) Handler {
+	return HandlerFunc(func(c *Context) {
+		h.ServeHTTP(c).ServeHTTP(c)
+	})
+}
+
+func resultHandlerSpreadAdapter(h func(context.Context, http.ResponseWriter, *http.Request) Handler) Handler {
+	return HandlerFunc(func(c *Context) {
+		h(c.Context, c.Response, c.Request).ServeHTTP(c)
 	})
 }
--- web/handler_test.go
+++ web/handler_test.go
@@ -14,7 +14,7 @@ var _ = Describe("ComposeMiddleware", func() {
 	It("should convert compatible middleware", func() {
 		i := 0
 		middleware := func(next web.Handler) web.Handler { i++; return next }
-		handlerFuncSignature := func(c context.Context, w http.ResponseWriter, r *http.Request) { i++ }
+		handlerFuncSignature := func(c *web.Context) { i++ }
 		handlerFunc := web.HandlerFunc(handlerFuncSignature)
 		handler := web.Handler(handlerFunc)
 		httpMiddleware := func(next http.Handler) http.Handler { i++; return next }
@@ -32,30 +32,63 @@ var _ = Describe("ComposeMiddleware", func() {
 			httpHandlerFunc,
 			httpHandlerFuncSignature,
 		)(handler).
-			ServeHTTP(nil, nil, nil)
+			ServeHTTP(&web.Context{})
 		Expect(i).To(Equal(9))
+	})
+
+	It("should panic if a middleware passed is nil", func() {
+		act := func() { web.ComposeMiddleware(nil) }
+		Expect(act).Should(Panic())
+	})
+
+	It("should panic if a middleware passed isn't convertable", func() {
+		act := func() { web.ComposeMiddleware(func() {}) }
+		Expect(act).Should(Panic())
 	})
 
 	It("should convert compatible handlers", func() {
 		i := 0
-		handlerFuncSignature := func(c context.Context, w http.ResponseWriter, r *http.Request) { i++ }
+		handlerFuncSignature := func(c *web.Context) { i++ }
 		handlerFunc := web.HandlerFunc(handlerFuncSignature)
 		handler := web.Handler(handlerFunc)
+		handlerSpreadFuncSignature := func(c context.Context, w http.ResponseWriter, r *http.Request) { i++ }
+		handlerSpreadFunc := web.HandlerSpreadFunc(handlerSpreadFuncSignature)
 		httpHandlerFuncSignature := func(w http.ResponseWriter, r *http.Request) { i++ }
 		httpHandlerFunc := http.HandlerFunc(httpHandlerFuncSignature)
 		httpHandler := http.Handler(httpHandlerFunc)
+		resultHandlerFuncSignature := func(c *web.Context) web.Handler { return handler }
+		resultHandlerFunc := web.ResultHandlerFunc(resultHandlerFuncSignature)
+		resultHandlerSpreadFuncSignature := func(c context.Context, w http.ResponseWriter, r *http.Request) web.Handler { return handler }
+		resultHandlerSpreadFunc := web.ResultHandlerSpreadFunc(resultHandlerSpreadFuncSignature)
 		handlers := []interface{}{
 			handler,
 			handlerFunc,
 			handlerFuncSignature,
+			handlerSpreadFuncSignature,
+			handlerSpreadFunc,
 			httpHandler,
 			httpHandlerFunc,
 			httpHandlerFuncSignature,
+			resultHandlerFuncSignature,
+			resultHandlerFunc,
+			resultHandlerSpreadFuncSignature,
+			resultHandlerSpreadFunc,
 		}
 		for _, h := range handlers {
-			web.ComposeMiddleware()(h).ServeHTTP(nil, nil, nil)
+			web.ComposeMiddleware()(h).
+				ServeHTTP(&web.Context{})
 		}
-		Expect(i).To(Equal(6))
+		Expect(i).To(Equal(12))
+	})
+
+	It("should panic if a handler passed is nil", func() {
+		act := func() { web.ComposeMiddleware()(nil) }
+		Expect(act).Should(Panic())
+	})
+
+	It("should panic if a handler passed isn't convertable", func() {
+		act := func() { web.ComposeMiddleware()(func() {}) }
+		Expect(act).Should(Panic())
 	})
 
 })
